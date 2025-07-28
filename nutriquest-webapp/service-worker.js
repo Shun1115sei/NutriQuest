8a3efe5b5834c02e0679e7205d3c15b8cc0fe3d5
Shun1115sei <shunseimiyasaka1115@gmail.com>
2025-07-28 20:48:49 +0800

    Update service-worker.js

--- nutriquest-webapp/service-worker.js
+++ nutriquest-webapp/service-worker.js
@@ -1,1 +1,36 @@
+// キャッシュの名前を定義
+const CACHE_NAME = 'nutriquest-cache-v1';
+// キャッシュするファイルのリスト
+const urlsToCache = [
+  './',
+  './index.html'
+  // アプリで他にキャッシュしたいファイルがあればここに追加します
+  // (例: './style.css', './app.js', './images/logo.png' など)
+];
 
+// サービスワーカーのインストール処理
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// リクエストへの応答処理
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // キャッシュにヒットすればそれを返す
+        if (response) {
+          return response;
+        }
+        // キャッシュになければネットワークから取得
+        return fetch(event.request);
+      }
+    )
+  );
+});
