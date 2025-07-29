05aa773ed4fb84d85c490ba1a48799e5b83810ac
Shun1115sei <shunseimiyasaka1115@gmail.com>
2025-07-29 13:19:29 +0800

    Update service-worker.js

--- nutriquest-webapp/service-worker.js
+++ nutriquest-webapp/service-worker.js
@@ -1,4 +1,4 @@
-// 2025-07-29 13:12:50: Updated file for cache busting
+// 2025-07-29 13:19:30: Updated file for cache busting
 // キャッシュの名前を定義
 const CACHE_NAME = 'nutriquest-cache-v1';
 // キャッシュするファイルのリスト
