3f9fa90137e4b082642cdba2b86b8fe888b95b8a
g6intdev <134787075+g6intdev@users.noreply.github.com>
2025-07-30 18:29:43 +0800

    edited vercel's outDir

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,7 @@ import { defineConfig } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
 export default defineConfig({
   plugins: [reactRouter()],
+  build: {
+    outDir: 'dist'
+  }
 });
